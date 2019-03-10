2b99280f9757035bddfb54b63f59185a2bcf51a1
TheCthaeh <andrewekimball@gmail.com>
2019-03-10 01:37:34 -0700

    Efficient implementation of chunking algorithm; solves all test cases.

--- main.py
+++ main.py
@@ -1,64 +1,108 @@
-import math
-
-def get_leftmost(b):
-    # Ex: 1101 -> 1000
-    return "1" + ("0" * (len(b)-1))
-
-def dec_to_hex(m):
-    string  = ""
-    hex_num = hex(int(m))
-    if (hex_num[0] == "-"):
+# Convert integer into hex string format for output
+def dec_to_hex(x):
+    string = ""
+    hex_num = hex(int(x))
+    if hex_num[0] == "-":
         string += "-"
     string += hex_num.split('x')[-1]
     return string.upper()
 
-def find_n_pos(m):
-    #Finds position numerically when m can be written as 2^n
-    n         = int(math.log(m, 2))
-    magnitude = pow(math.sqrt(2), n)
-    theta     = (math.pi/2) - ((math.pi/4)*n)
-    return [int(magnitude*math.cos(theta)), int(magnitude*math.sin(theta))]
 
-def find_coordinate(m):
+# Find the sum of the given array of lengths of binary numbers and return as int
+# Binary numbers always consist of a single '1' followed by a number of '0's
+def calc_sum(array):
+    bin_arr = []
+    sum = 0
+    if len(array) > 0:
+        bin_arr.append('0')
+    for n in array:
+        while n >= len(bin_arr):
+            bin_arr.append('0')
+        if bin_arr[n] == '1':
+            bin_arr[n] = '0'
+            bin_arr.append('1')
+        else:
+            bin_arr[n] = '1'
+    if len(bin_arr) > 0:
+        sum = int(''.join(bin_arr[::-1]), 2)
+    return sum
+
+
+# Subtract the x-array of the negative pos 2-d array from the x-array of the
+# positive pos 2-d array, and repeat with the corresponding y-arrays
+def calc_final_pos(p_pos, n_pos):
+    p_pos[0] = p_pos[0][::-1]
+    p_pos[1] = p_pos[1][::-1]
+    n_pos[0] = n_pos[0][::-1]
+    n_pos[1] = n_pos[1][::-1]
     pos = [0, 0]
-    # If m can be written as 2^n, go straight to find_n_pos
-    if (math.ceil(math.log(m, 2)) == math.floor(math.log(m, 2))):
-        pos = find_n_pos(m)
-    # Otherwise break binary form of m into chunks and solve recursively
-    else:
-        bin_m          = bin(m)[2:]
-        reached_middle = False
-        f_chunk        = ""
-        s_chunk        = ""
-        # Break m into chunks, Ex: 1100101 -> 1100000 and 101
-        for bit in bin_m:
-            if not reached_middle:
-                if (bit == "0"):
-                    reached_middle = True
-                f_chunk += bit
-            else:
-                f_chunk += "0"
-                s_chunk += bit
-        # Get pos for first chunk: pos of first bit - pos of other bits
-        pos = find_n_pos(int(get_leftmost(f_chunk), 2))
-        for i in range(1, len(f_chunk)):
-            if f_chunk[i] == "0":
-                break
-            t_pos = find_n_pos(int(get_leftmost(f_chunk[i:]), 2))
-            pos[0] -= t_pos[0]
-            pos[1] -= t_pos[1]
-        # Recursively find pos for second chunk, subtract from first chunk
-        if (len(s_chunk) > 0 and int(s_chunk, 2) != 0):
-            t_pos = find_coordinate(int(s_chunk, 2))
-            pos[0] -= t_pos[0]
-            pos[1] -= t_pos[1]
+    pos[0] = calc_sum(p_pos[0]) - calc_sum(n_pos[0])
+    pos[1] = calc_sum(p_pos[1]) - calc_sum(n_pos[1])
     return pos
 
-if __name__ == "__main__":
-    q = int(input())
-    for i in range(q):
-        n   = input()
-        m   = int(input().strip(), 16)
-        pos = find_coordinate(m)
-        print(dec_to_hex(pos[0]))
-        print(dec_to_hex(pos[1]))
+
+# Add (m - 1) % 2 to pos array based on angle around origin, where m
+# is the length of the binary number being considered in the chunking algorithm
+def find_n(m, p_pos, n_pos, polarity):
+    n = m - 1
+    x = n // 2
+    theta = n % 8
+    x_p = polarity
+    y_p = polarity
+    if 5 <= theta <= 7:
+        x_p *= -1
+    if 3 <= theta <= 5:
+        y_p *= -1
+    if n % 2 == 1:
+        if x_p == 1:
+            p_pos[0].append(x)
+        else:
+            n_pos[0].append(x)
+        if y_p == 1:
+            p_pos[1].append(x)
+        else:
+            n_pos[1].append(x)
+    elif n % 4 == 0:
+        if y_p == 1:
+            p_pos[1].append(x)
+        else:
+            n_pos[1].append(x)
+    elif n % 2 == 0:
+        if x_p == 1:
+            p_pos[0].append(x)
+        else:
+            n_pos[0].append(x)
+
+
+# Efficient implementation of chunking algorithm
+def get_pos(bin_m):
+    chunk_sign = 1
+    bit_sign = 1
+    flip = False
+    size = len(bin_m)
+    p_pos = [[], []]
+    n_pos = [[], []]
+    for i in range(size):
+        if bin_m[i] == '1':
+            if i > 0 and bin_m[i-1] == '0':
+                chunk_sign *= -1
+                bit_sign = 1
+                flip = True
+            polarity = chunk_sign * bit_sign
+            find_n(size - i, p_pos, n_pos, polarity)
+            if flip:
+                bit_sign = -1
+                flip = False
+            if i == 0:
+                bit_sign = -1
+    return calc_final_pos(p_pos, n_pos)
+
+
+# Read in input, get resulting position for each input and print
+q = int(input())
+for i in range(q):
+    input()
+    m = int(input(), 16)
+    pos = get_pos(bin(m)[2:])
+    print(dec_to_hex(pos[0]))
+    print(dec_to_hex(pos[1]))
